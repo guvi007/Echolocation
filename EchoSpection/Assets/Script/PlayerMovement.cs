2f55da40599db8921c0e8590c8f3e92c7d6a5b07
Kyzyl Monteiro <kyzylmonteiro@gmail.com>
2018-09-29 18:47:00 +0530

    Initial Commit : Working Prototype
    Features:
    Player movement
    Obstacle Spawning & Movement
    Player Gravity Management
    Visual Alarm for Proximity of Obstacle

---
+++ EchoSpection/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour {
+	public float playerSpeed;
+	public Vector2 pos;
+	public Rigidbody2D player;
+
+	// Use this for initialization
+	void Start () {
+		pos = transform.position;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		//Debug.Log (player.position.y);
+		/*
+		if (player.position.y > 0) {
+			player.AddForce (new Vector2(0,1f),ForceMode2D.Impulse );
+		} 
+		else if(player.position.y<0){
+			player.AddForce (new Vector2(0,-1f),ForceMode2D.Impulse );
+		}
+		*/
+		if (player.position.y > 0) {
+			pos.y += playerSpeed * Time.deltaTime/30;
+		}
+		else if(player.position.y==0){
+
+		}
+		else{
+			pos.y -= playerSpeed * Time.deltaTime/30;
+		}
+
+
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.Rotate(Vector3.back*Time.deltaTime*30f);
+		}
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Rotate(Vector3.forward*Time.deltaTime*30f);
+		}
+		pos.y += Input.GetAxis ("Vertical") * playerSpeed * Time.deltaTime;
+		if(pos.y>-4 && pos.y<4){
+			transform.position = pos;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D c){
+		if (c.gameObject.tag == "New tag" || c.gameObject.tag=="obstag") {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+			//Destroy (gameObject);
+
+		}
+	}
+}
